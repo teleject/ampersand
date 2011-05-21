b285e3c78cf8e3cf22168cabc5ec4b3cb7cd87bf
teleject <schmitt@christopher.org>
2011-05-20 19:22:48 -0500

    hacked script to include find and replace for double dashes, "--", to em dashes
    Signed-off-by: teleject <schmitt@christopher.org>

--- README.md
+++ README.md
@@ -6,9 +6,24 @@ A jQuery plugin to make use of the gorgeous Open Source Ampersands library: http
 
 It's a little jQuery plugin that works on any jQuery object and wraps ampersands in a span for separate styling.
 
+
+Friday; May 20, 2011 by Christopher Schmitt:
+
+Added ability to find and replace double dashes with open sace, " -- ", and replace them with an em dash and remove the white space. 
+
+More information on em dashes at http://www.alistapart.com/articles/emen/
+
 ### Why would I want to do that? ###
 
-Ampersands are cool, but the default ampersands in many fonts are a bit lame. Having really nice ampersands can lift a design. Check out http://webofawesome.com for an example.
+Ampersands are cool, but the default ampersands in many fonts are a bit lame. Having really nice ampersands can lift a design. 
+
+Check out http://webofawesome.com for an example.
+
+Friday; May 20, 2011 by CS:
+
+Double dashes in content are not that attractive, especially when you want to represent a sudden break in a conversation. A good ole, slick em dash does the trick. 
+
+Also, check out the included DEMO folder for a localized example of the script. 
 
 ## Usage ##
 
@@ -54,3 +69,14 @@ Obviously, replace path-to-font and myfont with the actual values.
 ## Limitations ##
 
 In it's current form, it will replace any ampersand, including ones inside hyperlinks. I should really get round to fixing this...
+
+Friday; May 20, 2011 by CS:
+
+Would need to split up the options to allow for finding and replacing specific characters in specific portions of a Web document. 
+
+E.g., maybe something like this:
+
+$("#main :header").ampersand( char : "&" ); // targets all HTML headers for ampersand upgrade
+$("p").ampersand( char : " -- " ); // targets all paragraphs for double dash replacements 
+
+
--- ampersand_plugin.js
+++ ampersand_plugin.js
@@ -1,24 +1,28 @@
 (function($){
-  $.fn.ampersand = function(ampClass) {
-    if (ampClass === undefined) {
-      ampClass = "ampersand";
-    }
-    var get_contents = function(el) {
-      $(el).contents().each(function(i,el) {
-        if (el.nodeType === 3) {
-          if (el.textContent) {
-            el.textContent = el.textContent.replace(/&/g, "#ampersandmarker#")
-          } else if (el.text) /* IE */ {
-            el.text = el.text.replace(/&/g, "#ampersandmarker#")
-          }
-        } else if (el.nodeType === 1) {
-          get_contents(el);
-        }
-      });
-    }
-    $(this).each(function(i, el) {
-      get_contents(el); /* Walk the dom finding only ampersands in text nodes. Don't bother splitting them. */
-      el.innerHTML = el.innerHTML.replace(/#ampersandmarker#/g, "<span class='"+ampClass+"'>&amp;</span>") /* replace the markers. */
+	$.fn.ampersand = function(ampClass) {
+		if (ampClass === undefined) {
+			ampClass = "amp";
+			emdashClass ="emdash";
+		}
+		var get_contents = function(el) {
+			$(el).contents().each(function(i,el) {
+				if (el.nodeType === 3) {
+					if (el.textContent) {
+						el.textContent = el.textContent.replace(/&/g, "#ampersandmarker#");
+						el.textContent = el.textContent.replace(/ -- /g, "#emdashmarker#")
+					} else if (el.text) /* IE */ {
+						el.text = el.text.replace(/&/g, "#ampersandmarker#")
+						el.text = el.text.replace(/ -- /g, "#emdashmarker#")
+					}
+				} else if (el.nodeType === 1) {
+					get_contents(el);
+				}
+			});
+		}
+	$(this).each(function(i, el) {
+		get_contents(el); /* Walk the dom finding only ampersands and em dashes in text nodes. Don't bother splitting them. */
+		el.innerHTML = el.innerHTML.replace(/#ampersandmarker#/g, "<span class='"+ampClass+"'>\&amp;</span>") /* replace the markers. */
+		el.innerHTML = el.innerHTML.replace(/#emdashmarker#/g, "<span class='"+emdashClass+"'>\&#8212;</span>") /* replace the markers. */
     });
     return this;
   };
